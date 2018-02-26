e53e13d26301abcfd57cdeafb5c7132c2979ceaa
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-02-26 14:27:34 +0100

    Fixing permissions on scripts.

---
+++ virbix/scripts/pools_list.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env ksh
+
+VIRSH="sudo `which virsh`"
+for vm in `${VIRSH} pool-list --all | egrep -v "(^-.*|^ Name.*|^$)" | awk '{print $1}'`; do
+    output=""
+    ${VIRSH} pool-info ${vm} | while read line; do
+       key=`echo ${line}|awk -F: '{print $1}'|awk '{$1=$1};1'`
+       val=`echo ${line}|awk -F: '{print $2}'|awk '{$1=$1};1'`
+       if [[ ${key} =~ ^(Id|Name|UUID|State)$ ]]; then
+          output+="`echo ${line}|awk -F: '{print $2}'|awk '{$1=$1};1'`|"
+       fi
+    done
+    echo ${output}|sed 's/.$//'
+done
