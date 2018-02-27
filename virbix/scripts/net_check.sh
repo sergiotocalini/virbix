535ab6a28ff9e0462cf41190060ec3b6c5489fef
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-02-27 09:47:36 +0100

    Adding net_check script.

---
+++ virbix/scripts/net_check.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env ksh
+
+APP_DIR=$(dirname $0)
+VIRSH="sudo `which virsh`"
+UUID="${1}"
+ATTR="${2}"
+TIMESTAMP=`date '+%s'`
+CACHE_DIR="${APP_DIR}/${CACHE_DIR:-./var/cache}/net"
+CACHE_FILE=${CACHE_DIR}/${UUID}.xml
+CACHE_TTL=5
+
+refresh_cache() {
+    [ -d ${CACHE_DIR} ] || mkdir -p ${CACHE_DIR}
+    if [[ -f ${CACHE_FILE} ]]; then
+	if [[ $(( `stat -c '%Y' "${CACHE_FILE}"`+60*${CACHE_TTL} )) -le ${TIMESTAMP} ]]; then
+	    ${VIRSH} net-dumpxml ${UUID} > ${CACHE_FILE}
+	fi
+    else
+	${VIRSH} net-dumpxml ${UUID} > ${CACHE_FILE}
+    fi
+}
+
+if [[ ${ATTR} == 'bridge_name' ]]; then
+    refresh_cache
+    rval=`xmllint --xpath "string(//network/bridge/@name)" ${CACHE_FILE}`
+elif [[ ${ATTR} == 'mac_addr' ]]; then
+    refresh_cache
+    rval=`xmllint --xpath "string(//network/mac/@address)" ${CACHE_FILE}`
+elif [[ ${ATTR} == 'active' ]]; then
+    rval="`${VIRSH} net-info ${UUID}|grep '^Active:'|awk -F: '{print $2}'|awk '{$1=$1};1'`"
+fi
+
+echo ${rval:-0}
