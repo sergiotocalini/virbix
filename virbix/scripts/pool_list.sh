897378daa975afebf4aebfa1a983fc179adff132
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2019-09-12 10:28:24 +0200

    Fixing duplicates uuid for pools, nets and domains

--- virbix/scripts/pool_list.sh
+++ virbix/scripts/pool_list.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env ksh
 
 VIRSH="sudo `which virsh`"
-for vm in `${VIRSH} pool-list --all | egrep -v "(^-.*|^ Name.*|^$)" | awk '{print $1}'`; do
+for vm in `${VIRSH} pool-list --all | egrep -v "(^-.*|^ Name.*|^$)" | awk '{print $1}' | sort | uniq`; do
     output=""
     ${VIRSH} pool-info ${vm} | while read line; do
        key=`echo ${line}|awk -F: '{print $1}'|awk '{$1=$1};1'`
